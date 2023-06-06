88eb9835012afa137457a04ef792a591de7a21a5
juan jose zabala <juanzabala317@hotmail.com>
2023-06-06 10:56:41 -0500

    versio6 layout

--- src/pages/Glosario.jsx
+++ src/pages/Glosario.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import Glosariolista from "../components/Glosariolista";
 import { Outlet } from "react-router-dom";
 
-const Glosario = () => {
+const Glosario = () => {    
   return (
     <>
       <motion.div
@@ -10,7 +10,7 @@ const Glosario = () => {
         bg-transparent grid grid-rows-[repeat(21,1fr)] grid-cols-[repeat(29,1fr)] max-xl:hidden
       xl:col-start-1 xl:col-end-[23] xl:row-start-1 xl:row-end-[12] xl:mb-3 xl:ml-3 xl:mt-3 xl:mr-3 xl:rounded-xl"
       >
-        <Outlet/>
+        <Outlet />
         <Glosariolista />
       </motion.div>
     </>
--- src/pages/Glosariomobil.jsx
+++ src/pages/Glosariomobil.jsx
@@ -1,16 +1,15 @@
 import { motion, AnimatePresence, easeInOut } from "framer-motion";
 import { useState } from "react";
 import Glosariolistamobil from "../components/Glosariolistamobil";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet,} from "react-router-dom";
 
 const Glosariomobil = () => {
-  const [isopen, setIsopen] = useState(null)
-  const location = useLocation()
-  console.log(location)
-
+  const [isopen, setIsopen] = useState(null);
+      
   return (
     <>
-      <Outlet/> {/*  eesto renderiza <Glosariocarpetamobil/> con react router dom */ }
+      <Outlet />
+      {/*  eesto renderiza <Glosariocarpetamobil/> con react router dom */}
       <motion.div
         whileTap={{ scale: 1.05 }}
         className="bg-white flex justify-center items-center col-start-2 col-end-[28] row-start-[21] row-end-[22] rounded-sm mb-1 z-0 outline
