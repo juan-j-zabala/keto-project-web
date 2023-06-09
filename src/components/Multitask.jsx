06c18f2f1e100159b27aa7111e0d27e5a029323b
juan jose zabala <juanzabala317@hotmail.com>
2023-06-09 11:26:26 -0500

    version9 layout

--- src/components/Multitask.jsx
+++ src/components/Multitask.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../context/ContextProvider";
+import { AnimatePresence, motion } from "framer-motion";
+import Svgimg from "./Svgimg";
+import close from "../assets/close-square-svgrepo-com.svg";
 
-const Multitask = ({data}) => {
+const Multitask = ({ data }) => {
+  const navigate = useNavigate();
+  const [, setOpen] = useContext(Context);
+
+  const [modalimg, setModalimg] = useState(null); // esto para el modal de imagenes con framer motion
+
   return (
     <>
       {data.title ? (
@@ -8,11 +18,51 @@ const Multitask = ({data}) => {
       ) : data.text ? (
         <>{data.text}</>
       ) : data.img ? (
-        <div className={`${data.css}`}>
-          <img className={`${data.imgcss}`} src={data.img} alt={data.alt} width={data.width} height={data.height} />
-        </div>
+        <>
+          <motion.div layoutId="1" className={`${data.css}`}>
+            <motion.img
+              whileHover={{ scale: 1.1 }}
+              onClick={() => setModalimg("1")}
+              className={`${data.imgcss}`}
+              src={data.img}
+              alt={data.alt}
+              width={data.width}
+              height={data.height}
+            />
+          </motion.div>
+          <AnimatePresence>
+            {modalimg ? (
+              <motion.div
+                className="absolute flex justify-center items-center bottom-0 right-0 bg-black w-full h-full bg-opacity-95"
+                layoutId={modalimg}
+              >
+                <img
+                  className="rounded-xl"
+                  src={data.img}
+                  alt={data.alt}
+                  // width=""
+                  // height=""
+                />
+                <button
+                  onClick={() => setModalimg(null)}
+                  className="bg-white rounded-lg absolute lg:right-3 lg:top-3
+                  max-lg:bottom-3"
+                >
+                  <Svgimg src={close} alt={"close"} width="40" height="40" />
+                </button>
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
+        </>
       ) : data.url ? (
-        <Link to={data.url} className={`${data.css}`} >{data.urltext}</Link>
+        <button
+          onClick={() => {
+            setOpen(data.hojacarpet), navigate(data.url);
+          }}
+          className={`${data.css}`}
+        >
+          {data.urltext}
+        </button>
       ) : data.br ? (
         <br />
       ) : null}
--- src/data/Datagrids.jsx
+++ src/data/Datagrids.jsx
@@ -24,7 +24,7 @@ export const objectdatagrids = [
             {
               id: 1,
               data: {
-                title: `esto es un titulo`,
+                title: ``,
                 text: "",
 
                 img: "",
@@ -33,11 +33,12 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
-                url: "",
-                urltext: "",
+                hojacarpet: "green",
+                url: "/glosario/carpet2",
+                urltext: "esto lleva aglosario carpet2",
 
                 br: null,
-                css: "",
+                css: "text-red-600",
               },
             },
           ],
@@ -66,17 +67,18 @@ export const objectdatagrids = [
                 title: ``,
                 text: "",
 
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
+                img: "../src/assets/Download_12.png",
+                imgcss: "rounded-xl",
+                alt: "bill-alish",
+                width: "400",
+                height: "400",
 
+                hojacarpet: "",
                 url: "",
                 urltext: "",
 
                 br: null,
-                css: "",
+                css: "flex justify-center",
               },
             },
           ],
@@ -111,6 +113,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -150,6 +153,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -189,6 +193,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -228,6 +233,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -267,6 +273,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -306,6 +313,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -345,6 +353,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -384,6 +393,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -423,6 +433,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
@@ -462,481 +473,7 @@ export const objectdatagrids = [
                 width: "",
                 height: "",
 
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-    ],
-  },
-  {
-    id: 1,
-    tema: "borrador",
-    url: "/tema2",
-    objectcssgrid: [
-      {
-        id: 1,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-3 row-end-[6]
-          sm:col-start-2 sm:col-end-11 sm:row-start-4 sm:row-end-[8] 
-          xl:rounded-xl xl:m-3 xl:col-start-8 xl:col-end-11 xl:row-start-3 xl:row-end-6`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "keto diet",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: `esto no es un titulo`,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 2,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-3 row-end-[6]
-          sm:col-start-[11] sm:col-end-[19] sm:row-start-4 sm:row-end-[8] 
-          xl:rounded-xl xl:m-3 xl:col-start-11 xl:col-end-[14] xl:row-start-3 xl:row-end-6`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "tema 2",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 3,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-6 row-end-[9]
-          sm:col-start-[19] sm:col-end-[28] sm:row-start-4 sm:row-end-[8] 
-          xl:rounded-xl xl:m-3 xl:col-start-[14] xl:col-end-[17] xl:row-start-3 xl:row-end-6`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 4,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-6 row-end-[9]
-          sm:col-start-2 sm:col-end-11 sm:row-start-[8] sm:row-end-[12] 
-          xl:rounded-xl xl:m-3 xl:col-start-[17] xl:col-end-[20] xl:row-start-3 xl:row-end-6`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 5,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-[9] row-end-[12]
-          sm:col-start-11 sm:col-end-[19] sm:row-start-[8] sm:row-end-[12] 
-          xl:rounded-xl xl:m-3 xl:col-start-8 xl:col-end-11 xl:row-start-6 xl:row-end-[9]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 6,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-[9] row-end-[12]
-          sm:col-start-[19] sm:col-end-[28] sm:row-start-[8] sm:row-end-[12] 
-          xl:rounded-xl xl:m-3 xl:col-start-11 xl:col-end-[14] xl:row-start-6 xl:row-end-[9]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 7,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-[12] row-end-[15]
-          sm:col-start-2 sm:col-end-11 sm:row-start-[12] sm:row-end-[16] 
-          xl:rounded-xl xl:m-3 xl:col-start-[14] xl:col-end-[17] xl:row-start-6 xl:row-end-[9]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 8,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-[12] row-end-[15]
-          sm:col-start-11 sm:col-end-[19] sm:row-start-[12] sm:row-end-[16] 
-          xl:rounded-xl xl:m-3 xl:col-start-[17] xl:col-end-[20] xl:row-start-6 xl:row-end-[9]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 9,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-[15] row-end-[18]
-          sm:col-start-[19] sm:col-end-[28] sm:row-start-[12] sm:row-end-[16] 
-          xl:rounded-xl xl:m-3 xl:col-start-8 xl:col-end-11 xl:row-start-[9] xl:row-end-[12]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 10,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-[15] row-end-[18]
-          sm:col-start-2 sm:col-end-11 sm:row-start-[16] sm:row-end-[20] 
-          xl:rounded-xl xl:m-3 xl:col-start-11 xl:col-end-[14] xl:row-start-[9] xl:row-end-[12]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 11,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-2 col-end-[15] row-start-[18] row-end-[21]
-          sm:col-start-11 sm:col-end-[19] sm:row-start-[16] sm:row-end-[20] 
-          xl:rounded-xl xl:m-3 xl:col-start-[14] xl:col-end-[17] xl:row-start-[9] xl:row-end-[12]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
-                url: "",
-                urltext: "",
-
-                br: null,
-                css: "",
-              },
-            },
-          ],
-        },
-      },
-      {
-        id: 12,
-        css: {
-          iconinfo: {
-            src: "",
-            alt: "",
-            width: "",
-            height: "",
-          },
-          generalcss: `flex justify-center items-center rounded-[3px] shadow-lg shadow-black bg-white col-start-[15] col-end-[28] row-start-[18] row-end-[21]
-          sm:col-start-[19] sm:col-end-[28] sm:row-start-[16] sm:row-end-[20] 
-          xl:rounded-xl xl:m-3 xl:col-start-[17] xl:col-end-[20] xl:row-start-[9] xl:row-end-[12]`,
-          cssopen: `grid grid-rows-[repeat(11,1fr)] grid-cols-[repeat(28,1fr)] bg-white col-start-1 col-end-[29] row-start-1 row-end-3 rounded-[3px] shadow-lg shadow-black
-          lg:rounded-[3px]
-          xl:row-end-4 xl:m-[3px] xl:mb-[11px] xl:rounded-xl`,
-          title: "",
-          content: [
-            {
-              id: 1,
-              data: {
-                title: ``,
-                text: "",
-
-                img: "",
-                imgcss: "",
-                alt: "",
-                width: "",
-                height: "",
-
+                hojacarpet: "green",
                 url: "",
                 urltext: "",
 
